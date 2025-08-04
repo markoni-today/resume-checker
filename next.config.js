fdaa9adf7776ff4b36cc828417293829b5c81dd9
markoni-today <156191053+markoni-today@users.noreply.github.com>
2025-08-04 22:17:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Настройки для деплоя на Vercel
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
